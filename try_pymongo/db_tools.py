a440b7a491bb22e02efdbf64f44bb28556bb7c0b
Štěfan Sakáloš <stefan.sakalos@tipsport.cz>
2019-07-17 16:21:13 +0200

    aaa

--- try_pymongo/db_tools.py
+++ try_pymongo/db_tools.py
@@ -6,70 +6,44 @@ from tempfile import TemporaryDirectory
 import shutil
 from pathlib import Path
 from typing import Dict
-from time import sleep
 
-def _zip_and_upload_to_gridfs(db:Database, path:str, info:Dict) -> ObjectId:
+def zip_and_upload(db:Database, path:str, info:Dict) -> ObjectId:
+    count = db.fs.files.count_documents(info)
+    assert count == 0, f"There are already {count} documents satisfying {info}."
+
     fs = gridfs.GridFS(db)
-    
-    
     with TemporaryDirectory() as tmpdir:
         zip_path = Path(tmpdir) / "tmp.zip"
         shutil.make_archive(Path(tmpdir) / "tmp", "zip", path)
         
         with open(zip_path, "rb") as zip_file:
-            _id = fs.put(zip_file, name=name or Path(path).name)
+            _id = fs.put(zip_file, **info)
             
     return _id
 
+def download_and_unzip(db:Database, out_dir:str, info:Dict) -> None:
+    count = db.fs.files.count_documents(info)
+    assert count == 1, f"There are {count} documents satisfying {info}."
+    _id = db.fs.files.find_one(info)['_id']
+    
+    fs = gridfs.GridFS(db)
+    with TemporaryDirectory() as tmpdir:
+        zip_path = Path(tmpdir) / "aaa.zip"
+        with fs.get(_id) as obj:
+            with open(zip_path, "wb") as zip_file:
+                zip_file.write(obj.read())
+                
+        shutil.unpack_archive(zip_path, out_dir)
 
-def _get_db(db:str, host:str="localhost", port:int=27017) -> Database:
+def _get_db(db:str, host:str=None, port:int=None) -> Database:
     client = MongoClient(host=host, port=port)
     return client[db]
-    
 
-def upload(path:str, db:Database,  
-           name:str=None, info:Dict=dict()) -> None:
-    
-    file_id = _zip_and_upload_to_gridfs(db, path, name)
-    
-    info = info.copy()
-    if "name" not in info:
-        info["name"] = name or Path(path).name
-    
-    info["file_id"] = file_id    
-    print(info)
-    
-    col = db["files_info"]
-    col.insert_one(info)
-    
-        
-        
-#%%
-
-path = r"E:\Pista\python_projects\workon_hrab2\bayes_sport_eager\data"
-db = _get_db("training_data")
-upload(path, db, info={"project": "test", "version": 0})
-
-#%%
-
-
-#%%
-path = Path(path)
-with open(path, "rb") as f:
-    a = fs.put(f, filename="data.pkl")
-    
-
-with fs.get(a) as obj:
-    with open("e:/tmp.pkl", "wb") as out_file:
-        out_file.write(obj.read())
-      
-##### zipping and unzipping
-#%%
-shutil.make_archive("e:/tmp2", "zip", r"E:\Pista\python_projects\workon_hrab2\bayes_sport_eager\data\tmp")      
-
-#%%
-from zipfile import ZipFile
-
-with ZipFile("e:/tmp.zip", 'w') as zf:
-    zf.write(r"E:\Pista\python_projects\workon_hrab2\bayes_sport_eager\data\tmp\handball\data.pkl")
-
+if __name__ == "__main__":  
+    #%%    
+    path = r"E:\Pista\python_projects\workon_hrab2\bayes_sport_eager\data"
+    db = _get_db("training_data")    
+    #%%
+    zip_and_upload(db, path, info={"project": "test", "version": 1})
+    #%%
+    download_and_unzip(db, out_dir="e:/tmp", info={"project": "test", "version": 1})
--- try_pymongo/try_GridFS.py
+++ try_pymongo/try_GridFS.py
@@ -8,21 +8,9 @@ fs = gridfs.GridFS(db)
 
 path = r"E:\Pista\python_projects\workon_hrab2\bayes_sport_eager\data\tmp\handball\data.pkl"
 with open(path, "rb") as f:
-    a = fs.put(f, filename="data.pkl")
+    a = fs.put(f, filename="data.pkl", info="whatever")
     
 
 with fs.get(a) as obj:
     with open("e:/tmp.pkl", "wb") as out_file:
         out_file.write(obj.read())
-      
-##### zipping and unzipping
-#%%
-import shutil
-shutil.make_archive("e:/tmp2", "zip", r"E:\Pista\python_projects\workon_hrab2\bayes_sport_eager\data\tmp")      
-
-#%%
-from zipfile import ZipFile
-
-with ZipFile("e:/tmp.zip", 'w') as zf:
-    zf.write(r"E:\Pista\python_projects\workon_hrab2\bayes_sport_eager\data\tmp\handball\data.pkl")
-
